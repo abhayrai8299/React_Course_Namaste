9f17d5aad998874f01cda9995eea8c08c1a435f2
Abhay Rai <abhay.rai8299@gmai.com>
2023-04-24 12:03:12 +0530

    episode-01

---
+++ first_example/App.js
@@ -0,0 +1,18 @@
+const heading=React.createElement("h1",{},"Hello world!");
+
+const parent=React.createElement("div",{id:"parent"},[
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"I'm an H1 tag!"),
+        React.createElement("h2",{},"I'm an H2 tag!"),
+    ]),
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"I'm an H1 tag!"),
+        React.createElement("h2",{},"I'm an H2 tag!"),
+    ])
+]);
+
+
+
+console.log(parent);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
